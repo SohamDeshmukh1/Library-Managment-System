d35568f4b33c45573d3fe512e50b7d8284538f0e
GamerzUnite <spdpvd3515@gmail.com>
2024-01-25 18:54:06 +0530

    Files was left to be added to work upon

---
+++ Components/_dbconnect.php
@@ -0,0 +1,13 @@
+<?php 
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+$conn = mysqli_connect($server, $username, $password, $database);
+
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+
+// echo "Connected successfully";
+// ?>
